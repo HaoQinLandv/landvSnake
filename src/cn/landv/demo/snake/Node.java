5142a929b668aee27322dfc51e3621b23dc86ad9
浩秦 <1358459281@qq.com>
2019-04-03 10:55:35 +0800

    msg

---
+++ src/cn/landv/demo/snake/Node.java
@@ -0,0 +1,21 @@
+package cn.landv.demo.snake;
+
+public class Node {
+    private final int x;
+    private final int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+}
