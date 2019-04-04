fa6a9de4c7223d56c04cc5f9db65e7cc8eb3f0c2
浩秦 <1358459281@qq.com>
2019-04-04 11:46:23 +0800

    2019-04-04

--- src/cn/landv/demo/snake/Grid.java
+++ src/cn/landv/demo/snake/Grid.java
@@ -1,5 +1,89 @@
 package cn.landv.demo.snake;
 
+/*
+ * Grid的数据成员
+ * 表达一个棋盘的所有状态
+ * 棋盘的方格是否被贪吃蛇覆盖
+ * 食物的位置在哪个方格
+ * 贪吃蛇目前的移动方向
+ * 一个Grid创建后，它的长宽就是固定不变了，方格的覆盖可以用一个Boolean类型的二维数组来表示，如果一个Node被贪吃蛇覆盖，则对应坐标的数组元素为true，否则false。
+ *Grid的构造函数
+ * 创建一个棋盘时，需要做一些必要的初始化工作，比如：
+ * 根据width和height初始化二维数组
+ * 初始化一条贪吃蛇
+ * 初始化食物
+ * 这些工作都可以在构造函数中完成，构造函数就是用来初始化一个类的地方。
+ * */
 public class Grid {
+    public final boolean status[][];
+    private final int width;
+    private final int height;
+
     private Snake sanke;
+
+    /*    public Grid(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }*/
+    //初始方向默认设置为向左。
+    private Direction snakeDirection = Direction.LEFT;
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+        status = new boolean[width][height];
+
+        initSnake();
+        createFood();
+    }
+
+    /*
+     * 接下来看initSnake和createFood如何实现
+     * 关键方法：初始化贪吃蛇
+     * 我们可以根据棋盘大小来创建一只大小合适的贪吃蛇，并将其房子棋盘的某些位置。
+     * 我们设定的规则如下：
+     * 贪吃蛇的长度为棋盘宽度的三分之一
+     * 贪吃蛇为水平放置，即包含的所有Node的Y坐标相同，Y坐标为棋盘垂直中间位置（即height/2),最左边的X为棋盘水平中间位置（即width/2）
+     * 所有initSnake()的代码逻辑如下：
+     */
+    private  Snake initSnake(){
+         snake = new Snake();
+        //设置Snake的body
+
+        //更新棋盘覆盖状态
+
+        return  snake;
+    }
+    /*
+    * 关键方法：随机创建食物
+    * 随机创建食物，即随机生成食物的X坐标和Y坐标。我们可以使用java提供的Randow类来生成随机数。
+    * 这里需要注意两点：
+    * 生成的X坐标和Y坐标必须在有效的范围之内，不能超过棋盘大小
+    * 食物的位置不能和贪吃蛇的位置重叠
+    * */
+    public Node createFood(){
+        int x,y;
+        //使用Randow设置X和Y
+         food = new Node(x,y);
+         return food;
+
+    }
+    /*
+    * 关键方法：一次移动
+    * 在Snake和move方法中，我们只是让贪吃蛇进行移动，移动方式是否有效以及移动后游戏是否继续并没有判断，
+    * 我们吧这些逻辑都放到Grid类的实现中，有Grid类来驱动Snake的move操作，
+    * Snake只管执行命令即可。
+    * 每一次移动可以认为是游戏的下一步，因此我们将这个函数定义为NextRound（）。
+    * 如果移动后能够继续，返回true，否则返回false。
+    * public boolean nextRound(){
+    *   按当前方向移动贪吃蛇
+    *
+    *   if(头部的位置是否有效){
+    *       把原来move操作时删除的尾部添加回来
+    *       创建一个新的食物
+    *   }
+    *   更新棋盘状态并返回游戏是否结束的标志
+    * }
+    *
+    * */
 }
--- src/cn/landv/demo/snake/Snake.java
+++ src/cn/landv/demo/snake/Snake.java
@@ -1,4 +1,56 @@
 package cn.landv.demo.snake;
 
+
+import java.util.LinkedList;
+/*
+* 为了表示相互连接在一起的节点，我们可以为Snake定义一个集合类型的成员变量，让集合来保存所有节点。
+* 你可能会说也可以使用数组来存储一组节点，但是数组的尺寸是固定的，通常情况下程序总是在运行时根据条件来创建对象，
+* 我们可能无法预知将要创建对象的个数（贪吃蛇的身体会不断变长），
+* 这时java的集合（Collection）类了（通常也称集合为容器）就是一个很好的选择，因为它们可以帮助我们方便地组织和管理一组对象。
+* 常用的集合类包括Map、List和Set，这里显然LIst是比较合适的，它提供了一系列操作一个元素序列的方法。
+* 接下来要考虑的问题是选择哪一种List，因为List也有许多种，常见的有ArrayList和LinkedList。这两者的主要不同在与：
+* ArrayList：通过下标随机访问元素快，但是插入、删除元素较慢。
+* LinkedList：插入、删除和移动元素快，但是通过下标随机访问元素性能较低。
+* 其实ArrayList是局域数组实现的，而LinkedList是基于连表实现的。这两种数据结构的特点决定了这两个容器的不同之处。
+* 结合我们自己的应用场景可以发现，贪吃蛇不断变长，经常做插入操作，而且我们不需要随机去访问贪吃蛇中的某一个节点。
+* 因此，果断选择LinkedList。
+* 有了这个思考过程，接下来Snake的成员变量就很清晰了
+*
+* Snake应该提供什么方法来操作自己的状态呢？贪吃蛇有两种情况下会有状态的变化，一种是迟到食物的时候，一种就是做了一次移动的时候。
+* 此外，贪吃蛇也需要定一些查询自己状态和信息的公有方法。比如获取贪吃蛇的头部，获取贪吃蛇的body，对应可以加入这些方法。
+* 一开始可能定义的方法不够完整，没关系，在编码过程中你会很自然地发现需要Snake提供更多的方法来完成特定功能，这个时候你再添加即可。
+* 把这些方法加入进去之后，Snake的代码看起来就丰富多了：
+* */
 public class Snake {
+    private LinkedList<Node> body = new LinkedList<>();
+
+    public  Node eat(Node food){
+        //如果food与头部相邻，则将food这个Node加入到body中，返回food
+        //否则不做任何操作，返回null
+
+        if (isNeighbor(body.getFirst(), food)) {
+            // 相邻情况下的处理
+        }
+    }
+
+    private boolean isNeighbor(Node a, Node b) {
+        return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY()) == 1;
+    }
+
+    public  Node move(Direction direction){
+        //根据方向更新贪吃蛇的body
+        //返回移动之前的尾部Node
+    }
+
+    public  Node getHead(){
+        return  body.getFirst();
+    }
+    public  Node addTail(Node area){
+        this.body.addLast(area);
+        return area;
+    }
+    public  LinkedList<Node> getBody(){
+        return  body;
+    }
+
 }
